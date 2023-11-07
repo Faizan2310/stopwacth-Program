fb627cad596f74b87c57121d4717ffcad1cbfebe
Faizan2310 <210057917@aston.ac.uk>
2023-11-07 17:45:54 +0000

    start btn updated

--- index.js
+++ index.js
@@ -4,15 +4,44 @@ const startBtn = document.querySelector("#startBtn");
 const pauseBtn = document.querySelector("#pauseBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
-let StartTime = 0;
+let startTime = 0;
 let elapsedTime = 0;
 let currentTime = 0;
-let pause = true;
+let paused = true;
 let intervalId;
 let hrs = 0;
 let mins = 0;
 let secs = 0;
  
 startBtn.addEventListener("click", ()=>{
-       
-});
+       if(paused){
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime,1000)
+       }
+}); 
+
+pauseBtn.addEventListener("click", ()=>{
+    if(!paused){
+        paused = true
+        elapsedTime = Date.now() + startTime;
+        clearInterval(intervalId);
+    }
+})
+
+function updateTime(){
+    elapsedTime = Date.now() - startTime; 
+    secs = Math.floor((elapsedTime/1000)%60);
+    mins = Math.floor((elapsedTime/(1000*60))%60);
+    hrs = Math.floor((elapsedTime/(1000*60*60))%60);
+
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`
+
+    function pad(unit){
+        return(("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+}
