fa527ffaf80003d2cee9119cec21a86cd582c525
Faizan2310 <210057917@aston.ac.uk>
2023-11-07 16:31:46 +0000

    added some JS

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const timeDisplay = document.querySelector("#timeDisplay");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let StartTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let pause = true;
+let intervalId;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+ 
+startBtn.addEventListener("click", ()=>{
+       
+});
